50e34b3a69ac4f6dd250e0747d5d1ac943984472
Ruggeri Raúl Guillermo <ruggeriguillermo@gmail.com>
2022-10-08 21:31:47 -0300

    PowerUp Shield funcionando beta

--- Assets/Scripts/PowerUp/PowerUpShield.cs
+++ Assets/Scripts/PowerUp/PowerUpShield.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PowerUpShield : MonoBehaviour
 {
+    // Se debe cambiar el MAXSHILD cuando se camnbie por inspector el valor de DurationShield
+    public const float MAXSHIELD = 10;
     public float shieldDuration;
     public GameObject Shield;
     public GameObject effect;
     private WaitForSeconds shieldDelay;
+
+    // Creo un evento que se dispará cuando comienza la corrutina y acciona la descarga de PowerUp
+    public static event Action OnShieldActive;
 
     // Start is called before the first frame update
     void Start()
@@ -16,17 +22,20 @@ public class PowerUpShield : MonoBehaviour
         shieldDelay = new WaitForSeconds(shieldDuration);
         //Shield.GetComponent<MeshRenderer>().enabled = false;
         //Shield.GetComponent<Collider>().enabled = false;
-        shieldDuration = 5f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        shieldDuration -= Time.deltaTime;
+        shieldDuration = Math.Clamp(shieldDuration, 0, MAXSHIELD);
     }
     public void ShieldUp()
     {
+        shieldDuration = MAXSHIELD;
         StartCoroutine(EngageShield());
+        // se dispara el evento
+        PowerUpShield.OnShieldActive.Invoke();
         Debug.Log("Shield Up");
     }
 
