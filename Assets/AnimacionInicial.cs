bb848a0dd28014a602bbd968ddfb666b3c313a2c
Ruggeri Raúl Guillermo <ruggeriguillermo@gmail.com>
2022-10-09 20:55:48 -0300

    Update AnimacionInicial.cs

--- Assets/AnimacionInicial.cs
+++ Assets/AnimacionInicial.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class AnimacionInicial : MonoBehaviour
 {
+    [SerializeField] GameObject CamDolly;
+    [SerializeField] GameObject HUD;
+    [SerializeField] GameObject CamPlayer;
+    [SerializeField] Animator Player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(Cinematica1());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator Cinematica1()
+    {
+        CamDolly.SetActive(true);
+        yield return new WaitForSeconds(15);
+        CamPlayer.SetActive(true);
+        yield return new WaitForSeconds(1);
+        Player.SetTrigger("SpawnPlayer");
+        HUD.SetActive(true);
     }
 }
