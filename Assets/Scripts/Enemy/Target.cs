0e6a90f7411918a9762c4476223c767d68a62940
xtemparmonkx <gastondavids@gmail.com>
2022-10-10 00:23:44 -0300

    lanzallamas

--- Assets/Scripts/Enemy/Target.cs
+++ Assets/Scripts/Enemy/Target.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Target : MonoBehaviour
+public class Target : MonoBehaviour, IDamageable, IBurnable
 {
-    [SerializeField] public int vidaMax;
+    
+    public bool IsBurning { get => _IsBurning; set => _IsBurning = value; }
     public float vidaActual;
+    [SerializeField] public int vidaMax;    
     [SerializeField] Image barraVida;
     [SerializeField] GameObject enemigo;
     [SerializeField] GameObject humo;
     [SerializeField] GameObject fuego;
     [SerializeField] GameObject explosion;
+    [SerializeField]
+    private bool _IsBurning;
+    private float _Health;
+    private Coroutine BurnCoroutine;
 
 
     private void Start()
@@ -69,8 +75,42 @@ public class Target : MonoBehaviour
 
     void Die()
     {
-        // enemigo.GetComponent<MovimientoInferior2>().enabled = false;
+        // enemigo.GetComponent<MovimientoInferior2>().enabled = false;        
         // enemigo.GetComponentInChildren<TurretControl>().enabled = false;
-        Destroy(gameObject);        
+        explosion.SetActive(true);
+        Destroy(gameObject);
+    }
+    public void StartBurning(int DamagePerSecond)
+    {
+        IsBurning = true;
+        if (BurnCoroutine != null)
+        {
+            StopCoroutine(BurnCoroutine);
+        }
+
+        BurnCoroutine = StartCoroutine(Burn(DamagePerSecond));
+    }
+    private IEnumerator Burn(int DamagePerSecond)
+    {
+
+        Debug.Log("target");
+        float minTimeToDamage = 1f / DamagePerSecond;
+        WaitForSeconds wait = new WaitForSeconds(minTimeToDamage);
+        int damagePerTick = Mathf.FloorToInt(minTimeToDamage) + 2;
+
+        ApplyDamage(damagePerTick);
+        while (IsBurning)
+        {
+            yield return wait;
+            ApplyDamage(damagePerTick);
+        }
+    }
+    public void StopBurning()
+    {
+        IsBurning = false;
+        if (BurnCoroutine != null)
+        {
+            StopCoroutine(BurnCoroutine);
+        }
     }
 }
